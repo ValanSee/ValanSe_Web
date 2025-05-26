77231b68dce9c47efb907869526483b209457563
topeanut <ljhh1648@gmail.com>
2025-05-26 19:44:13 +0900

    redux & folder setting

---
+++ src/hooks/redux.ts
@@ -0,0 +1,7 @@
+//예시
+// const dispatch = useAppDispatch()
+// dispatch(
+//   login({ nickname: '홍길동', gender: '남', ageRange: '20대', mbti: 'I' }),
+// )
+
+// const nickname = useAppSelector((state) => state.user.nickname)
