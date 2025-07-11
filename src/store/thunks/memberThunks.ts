df935f880bea1b3b35ffdeb1c5f0b3cd4d04fff8
Emithen <86219540+Emithen@users.noreply.github.com>
2025-07-12 06:22:14 +0900

    feat: member 미들웨어 구축

--- src/store/thunks/memberThunks.ts
+++ src/store/thunks/memberThunks.ts
@@ -1,5 +1,5 @@
-import { fetchMemberProfile } from '@/api/member'
-import { setProfile } from '../slices/memberSlice'
+import { fetchMemberMypage, fetchMemberProfile } from '@/api/member'
+import { setProfile, setMypageData } from '../slices/memberSlice'
 import { AppDispatch } from '../store'
 
 // 프로필을 가져오고 store에 저장
@@ -8,6 +8,19 @@ export const fetchProfileThunk = () => async (dispatch: AppDispatch) => {
     const profile = await fetchMemberProfile()
     dispatch(setProfile(profile))
     return profile
+  } catch (err) {
+    throw err
+  }
+}
+
+// 마이페이지 데이터 가져오고 store에 저장
+export const fetchMypageDataThunk = () => async (dispatch: AppDispatch) => {
+  try {
+    const mypageData = await fetchMemberMypage()
+    if (mypageData) {
+      dispatch(setMypageData(mypageData))
+    }
+    return mypageData
   } catch (err) {
     throw err
   }
