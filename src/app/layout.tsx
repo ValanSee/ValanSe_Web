e95dba439c025c84b7a6b4b812a64b893174b7b9
Emithen <86219540+Emithen@users.noreply.github.com>
2025-07-12 14:26:23 +0900

    fix: linting 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import Providers from './providers'
-import { ModalRootInitializer } from './modalRootInitializer'
 
 export const metadata: Metadata = {
   title: 'ValanSe',
