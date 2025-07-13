4079f73513812c3e43904cbe7b9b532cf865b5ce
Emithen <86219540+Emithen@users.noreply.github.com>
2025-07-13 19:02:41 +0900

    chore: pnpm-lock 파일 일치

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import Providers from './providers'
+import { ModalRootInitializer } from './modalRootInitializer'
 import AuthRedirectWatcher from './authRedirectWatcher'
 
 export const metadata: Metadata = {
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body>
         <Providers>
           <AuthRedirectWatcher />
+          <ModalRootInitializer />
           {children}
         </Providers>
       </body>
