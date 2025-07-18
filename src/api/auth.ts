1861512ef76db64565c2f093ecb0e3de908044a5
Emithen <86219540+Emithen@users.noreply.github.com>
2025-07-18 15:46:48 +0900

    refactor: auth api 코드 리팩토링

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,11 +1,26 @@
+// api instance
 import { authApi } from './instance/authApi'
+import { publicApi } from './instance/publicApi'
+// types
+import { LoginResponse, ReissueResponse } from '@/types/poll/api/auth'
+
+export const login = async (code: string) => {
+  try {
+    const response = await publicApi.post<LoginResponse>('/auth/kakao/login', {
+      code,
+    })
+    return response.data
+  } catch (error) {
+    throw error
+  }
+}
 
 export const reissue = async (refreshToken: string) => {
   try {
-    const response = await authApi.post<string>('/auth/reissue', {
+    const response = await authApi.post<ReissueResponse>('/auth/reissue', {
       refreshToken,
     })
-    return response.data
+    return response.data.accessToken
   } catch (error) {
     throw error
   }
