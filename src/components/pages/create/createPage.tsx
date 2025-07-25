6ee7be6942372338a1f769c620338b1d90ffb7cc
Emithen <86219540+Emithen@users.noreply.github.com>
2025-07-25 20:10:51 +0900

    fix: suspense 추가

--- src/components/pages/create/createPage.tsx
+++ src/components/pages/create/createPage.tsx
@@ -2,10 +2,11 @@
 
 import BottomNavBar from '@/components/_shared/bottomNavBar'
 import CreateForm from './createForm'
+import { Suspense } from 'react'
 
 const CreatePage = () => {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <div className="flex flex-col items-center pt-6 bg-white min-h-screen pb-20">
         <div className="flex items-center justify-start w-full px-4 py-2.5">
           <div className="text-[20px] font-[700] leading-none">
@@ -18,7 +19,7 @@ const CreatePage = () => {
         </div>
       </div>
       <BottomNavBar />
-    </>
+    </Suspense>
   )
 }
 
